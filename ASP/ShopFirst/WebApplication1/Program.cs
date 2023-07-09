4fc66c9fa0e1621c9f33fccff32aa7b1a4bb49e6
Polina Avsukevich <polina.avs@gmail.com>
2023-07-09 14:32:42 +0300

    added

--- ASP/ShopFirst/WebApplication1/Program.cs
+++ ASP/ShopFirst/WebApplication1/Program.cs
@@ -1,10 +1,22 @@
+using WebApplication1;
+using WebApplication1.Data.interfaces;
+using WebApplication1.Data.Moks;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddRazorPages();
+
+builder.Services.AddMvc();
+builder.Services.AddTransient<IAllCars, MockCars>(); //какой класс и какой интерфес реализует 
+builder.Services.AddTransient<ICarsCategory, MockCategory>();
+
+
+
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();//для отображения ошибок
 app.UseStatusCodePages(); //для отображение кодов стр (404)
 app.UseStaticFiles();//для картинок и тд
-app.UseMvcWithDefaultRoute(); //отображение url адреса по умолчанию
+//app.UseMvcWithDefaultRoute(); //отображение url адреса по умолчанию
 
 //app.MapGet("/", () => "Hello World!");
 
---
+++ ASP/ShopFirst/WebApplication1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication1.Data.interfaces;
+using WebApplication1.Data.Moks;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IAllCars, MockCars>();
+
+       
+        }
+    }
+}
