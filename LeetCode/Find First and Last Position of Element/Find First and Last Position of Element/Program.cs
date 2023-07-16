8e81d3846413a82ac120b4a04e2fdb2518901233
Polina Avsukevich <polina.avs@gmail.com>
2023-07-16 15:01:56 +0300

    added

---
+++ LeetCode/Find First and Last Position of Element/Find First and Last Position of Element/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int BinarySearch(int[] mas, int search, int first, int last )
+        {
+            int rez = -1;
+            if (first>=last)
+            {
+                if (first> mas.Length-1) {
+                    return -1;
+                }
+                if (mas[first] == search)
+                    return first;
+                return -1;
+            }
+            var center = (last + first) / 2;
+            if (mas[center]==search)
+            {
+                return center;
+            }
+            if (mas[center]<search)
+            {
+               rez= BinarySearch(mas, search, center+1, last);
+            }
+            if (mas[center] > search)
+            {
+              rez=  BinarySearch(mas, search, first, center-1);
+            }
+            return rez;
+         
+        }
+
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            int[] result = { -1,-1};
+            if (nums.Length == 0)
+            {
+  
+                return result;
+            }
+
+
+           int center= BinarySearch(nums, target, 0, nums.Length);
+            if (center != -1)
+            {
+                result[0] = center;
+                result[1] = center;
+                int right = BinarySearch(nums, target, center , nums.Length);
+                int left = BinarySearch(nums, target, 0, center);
+                if (left != -1)
+                {
+                    result[0] = left;
+                } 
+                if (right!=-1) {
+                  
+                    result[1] = right;
+                }
+             
+            }
+         
+               
+           
+
+          
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = {2,2 };
+            int target = 3;
+            int[] ints = SearchRange(nums, target);
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
---
+++ LeetCode/linked_list/linked_list/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class ListNode
+    {
+     public int val;
+      public ListNode next;
+      public  ListNode head; // головной/первый элемент
+        ListNode? tail; // последний/хвостовой элемент
+       public static int count;  // количество элементов в списке
+                    // добавление элемента
+        public void Add(int val)
+        {
+            ListNode node = new ListNode(val);
+
+            if (head == null)
+                head = node;
+            else
+                tail.next = node;
+            tail = node;
+
+            count++;
+        }
+        public void Remove()
+        {
+
+        }
+
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+           this.val = val;
+           this.next = next;
+                 }
+ }
+
+    class Program
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+
+
+            ListNode? current = head;
+            ListNode? previous = null;
+            int a = 0;
+
+            while (current != null && a != ListNode.count-n)
+            {
+                a++;
+                previous= current;
+                current = current.next;
+            }
+            previous.next = current.next;
+
+
+            return head;
+                    
+            
+
+            /*   var elem = head;
+               int amount = 1;
+              for(int i=0; i< ListNode.count - n; i++)
+               {
+                   elem = elem.next;
+               }
+               Console.WriteLine("numer del" + elem.val);
+
+
+
+
+
+               return elem;*/
+        }
+        static void Main(string[] args)
+        {
+
+            int[] head = { 1, 2, 3, 4, 5 };
+            int n = 2;
+          ListNode list = new ListNode();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            var elem = list.head;
+            while (elem != null)
+            {
+                Console.WriteLine(elem.val);
+                elem = elem.next;
+            }
+            Program pr = new Program();
+           ListNode lst= pr.RemoveNthFromEnd(list.head, n);
+            elem = list.head;
+            while (elem != null)
+            {
+                Console.WriteLine(elem.val);
+                elem = elem.next;
+            }
+
+
+
+
+
+        }
+    }
+}
