1acf7b95bc6bad2a1faf1f2dc2bb2ed5bfee219b
Polina Avsukevich <polina.avs@gmail.com>
2023-07-19 10:47:55 +0300

    added to the repo

---
+++ Paterns/Observable/Observable/Observers/TempretureCondition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observable.Observers
+{
+    public class TempretureCondition : IObserver, IDislayElement
+    {
+        private float temp;
+        private float humidity;
+        private ISubject weatherData;
+        public TempretureCondition(ISubject weather)
+        {
+            this.weatherData = weather;
+            weatherData.registerObserver(this);//решистрация элемента в кач наблюдателя
+        }
+        public void display()
+        {
+            Console.WriteLine("Температура:" + temp +"TempretureCond");
+        }
+
+        void IObserver.update(float temp, float humidity)
+        {
+            this.temp = temp;
+            this.humidity = humidity;
+            display();
+        }
+    }
+}
--- Paterns/Observable/Observable/Program.cs
+++ Paterns/Observable/Observable/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Observable.Observers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,14 @@ namespace Observable
         static void Main(string[] args)
         {
            WheatherData wheatherData = new WheatherData();//объект
-            CurrentCondition currentCondition = new CurrentCondition(wheatherData);
+            CurrentCondition currentCondition = new CurrentCondition(wheatherData);//создаем объект, который подписан на обновления weatherData
+            TempretureCondition tempretureCondition = new TempretureCondition(wheatherData);
+
+
             wheatherData.setMeasurements(12,22);
             wheatherData.setMeasurements(22, 32);
+
+
         }
     }
 }
