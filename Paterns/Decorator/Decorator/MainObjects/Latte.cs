0e3c20b6ce09a9e9de6e2840a05f5ac77754e9a2
Polina Avsukevich <polina.avs@gmail.com>
2023-07-20 11:07:33 +0300

    added

---
+++ Paterns/Decorator/Decorator/MainObjects/Latte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.MainObjects
+{
+    public class Latte : CofeShop
+    {
+        public Latte()
+        {
+            description = "Latte";
+        }
+        public override double cost()
+        {
+            return 2.99;
+        }
+    }
+}
