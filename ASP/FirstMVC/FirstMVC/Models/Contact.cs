7d68b61d6e44e9be4dc5343833ac491e3f6c59c6
Polina Avsukevich <polina.avs@gmail.com>
2023-08-05 13:07:22 +0300

    added

---
+++ ASP/FirstMVC/FirstMVC/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstMVC.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public int Age { get; set; }
+
+    }
+}
