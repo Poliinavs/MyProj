2792db2fc41691326572e05c5b4ca6b4b49a4a14
Polina Avsukevich <polina.avs@gmail.com>
2023-08-05 12:54:43 +0300

    added

---
+++ ASP/FirstMVC/FirstMVC/Controllers/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMVC.Controllers
+{
+    public class ContactsController:Controller
+    {
+        public IActionResult Index() //при переходе на Contacts срабатывает данный метод
+        {
+            return View();
+        }
+
+    }
+
+}
---
+++ ASP/FirstMVC/FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() //вызов главной страницы index.cshtml
+        {
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
