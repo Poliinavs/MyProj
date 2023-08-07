33874135bb6dfee124f886bafa6f99665dccfd4e
Polina Avsukevich <polina.avs@gmail.com>
2023-08-07 17:10:28 +0300

    added

---
+++ Paterns/singleton/singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Single fr= Single.getInstance();
+            fr.Inform();
+        }
+    }
+}
---
+++ Paterns/singleton/singleton/Single.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    public class Single
+    {
+        private static Single UniqueInstance; //для хранения единственного экземпляра 
+        private Single() { }
+        public static Single getInstance() { 
+            if (UniqueInstance == null)
+        return new Single();
+            return UniqueInstance;
+        }
+        public void Inform()
+        {
+            Console.WriteLine("Одиночка!");
+        }
+
+    }
+}
